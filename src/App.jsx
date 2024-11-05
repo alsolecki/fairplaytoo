4355e82e4f0ed68dec8eda97541009e7b2325997
Alex Solecki <129791664+alsolecki@users.noreply.github.com>
2024-11-04 21:07:22 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,211 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Fair Play at 9911</h1>
+      <div className="home">
+        <h2>Home</h2>
+        <div className="group">
+          <div className="player">
+            <h3>Lisa</h3>
+            <div class="tasks">
+              <p>Laundry</p>
+              <p>Home Furnishings</p>
+              <p>Memories and Photos</p>
+              <p>Childcare Helpers</p>
+              <p>Cleaning</p>
+              <p>Money Manager</p>
+              <p>Tidying Up, Organization and Donation</p>
+              <p>Storage, Garage and Seasonal Items</p>
+            </div>
+          </div>
+          <div class="player">
+            <h3>tbd</h3>
+            <div class="tasks">
+              <p>Dry Cleaning?</p>
+              <p>Groceries</p>
+              <p>Home Goods and Supplies</p>
+              <p>Home Maintainance</p>
+              <p>Home Mortgage/Insurance </p>
+              <p>Hosting</p>
+              <p>Lawn & Plants</p>
+              <p>Meals, weekday breakfast</p>
+              <p>Meals, weekday dinner</p>
+              <p>Meals, weekend</p>
+            </div>
+          </div>
+          <div class="player">
+            <h3>Alex</h3>
+            <div class="tasks">
+              <p>Garbage</p>
+              <p>Dishes</p>
+              <p>Mail</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="care">
+        <h2>Caregiving</h2>
+        <div className="group">
+          <div class="player">
+            <h3>Lisa</h3>
+            <div class="tasks">
+              <p>Grooming & Wardrobe, Player 1</p>
+              <p>Self Care, player 1</p>
+              <p>Birth Control</p>
+              <p>Friendship and Social Media, kids</p>
+              <p>Bathing and Grooming, kids</p>
+              <p>Clothes and Acccessories, kids </p>
+              <p>Diaper and Potty Training, kids</p>
+              <p>Teacher Communication, kids</p>
+              <p>Dental, kids</p>
+              <p>Homework & School Supplies, kids</p>
+              <p>School Services, kids</p>
+              <p>Medical & Healthy Living, kids</p>
+              <p>Health Insurance</p>
+            </div>
+          </div>
+          <div class="player">
+            <h3>TBD</h3>
+            <div class="tasks">
+              <p>Pets</p>
+              <p>Estate Planning and Life Insurance</p>
+              <p>Morning Routine, kids</p>
+              <p>Parents & In-Laws</p>
+              <p>School Transitions, kids</p>
+              <p>Medical Needs & Mental Health, kids</p>
+              <p>School Transitions, kids</p>
+            </div>
+          </div>
+          <div class="player">
+            <h3>Alex</h3>
+            <div class="tasks">
+              <p>Grooming & Wardrobe, Player 2</p>
+              <p>Self Care, player 2</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="out">
+        <h2>Out</h2>
+        <div className="group">
+          <div class="player">
+            <h3>Lisa</h3>
+            <div class="tasks">
+              <p>Birthday Celebrations, other kids</p>
+              <p>Calander Keeper</p>
+              <p>Packing & Unpacking, local kids</p>
+              <p>Packing & Unpacking, travel</p>
+              <p>Points, miles, coupons</p>
+              <p>Returns & store credits</p>
+              <p>Social Plans, couples</p>
+              <p>Travel</p>
+              <p>Tutoring & Coaching, kids</p>
+              <p>Weekend Plans, family</p>
+            </div>
+          </div>
+          <div class="player">
+            <h3>TBD</h3>
+            <div class="tasks">
+              <p>Cash & Bills</p>
+              <p>Charity?</p>
+              <p>Civic Engagement & Cultural Enrichment</p>
+              <p>Electronics and IT</p>
+              <p>Extracurricular, non-sports kids</p>
+              <p>Extracurricular, sports kids</p>
+              <p>First Aid, Saftey and Safety</p>
+              <p>Auto</p>
+              <p>Social Plans, couples</p>
+              <p>Transportation, kids</p>
+              <p>School Forms, kids</p>
+              <p>School Breaks, kids non-summer</p>
+              <p>School Breaks, kids summer</p>
+            </div>
+          </div>
+          <div class="player">
+            <h3>Alex</h3>
+            <div class="tasks">
+              <p>Dishes</p>
+              <p>Mail</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="magic">
+        <h2>Magic</h2>
+        <div className="group">
+          <div class="player">
+            <h3>Lisa</h3>
+            <div class="tasks">
+              <p>Adult Friendships, player 1</p>
+              <p>Birthday Celebration, own kids</p>
+            </div>
+          </div>
+          <div class="player">
+            <h3>TBD</h3>
+            <div class="tasks">
+              <p>Extended Family</p>
+              <p>Fun and Playing, kids</p>
+              <p>Gestures of Love, kids</p>
+              <p>Gifts, family</p>
+              <p>Gifts, VIPs</p>
+              <p>Holiday Cards</p>
+              <p>Holidays</p>
+              <p>Informal Education, kids</p>
+              <p>Magic Beings, kids</p>
+              <p>Marriage & Romance</p>
+              <p>Middle of the night comfort, kids</p>
+              <p>Partner Coach?</p>
+              <p>Showing Up and Participating, kids</p>
+              <p>Watching, kids</p>
+              <p>Dicipline & Screentime, kids</p>
+              <p>Hard Questions, kids</p>
+              <p>Spirituality</p>
+            </div>
+          </div>
+          <div class="player">
+            <h3>Alex</h3>
+            <div class="tasks">
+              <p>Adult Friendships, player 2</p>
+              <p>Thank You Notes</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="wild">
+        <h2>Wild</h2>
+        <div className="group">
+          <div class="player">
+            <h3>Lisa</h3>
+            <div class="tasks">
+              <p>Unicorn Space</p>
+              <p>Aging/Ailing parent</p>
+            </div>
+          </div>
+          <div class="player">
+            <h3>TBD</h3>
+            <div class="tasks">
+              <p>Moving</p>
+              <p>First year of infant's life</p>
+              <p>Death</p>
+              <p>Glitch in Matrix, Daily disruption</p>
+              <p>Welcoming new child to home</p>
+              <p>Home Renovation</p>
+              <p>Serious Illness</p>
+              <p>Job Loss / Money problems</p>
+              <p>New Job</p>
+            </div>
+          </div>
+          <div class="player">
+            <h3>Alex</h3>
+            <div class="tasks">
+              <p>Unicorn Space</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
